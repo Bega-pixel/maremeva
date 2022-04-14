7cc4e49474bff84687d9e685a7b722c21e388cae
Bega-pixel <bega2000@protonmail.com>
2022-04-14 09:49:42 +0200

    fix responsive, smart phone bug

--- maremeva/src/components/cover/Cover.js
+++ maremeva/src/components/cover/Cover.js
@@ -5,6 +5,7 @@ const Cover = () => {
   return (
     
       <div className='cover-container'>
+    
         <img className='cover-img' src="https://res.cloudinary.com/dazypdtba/image/upload/v1647432144/Mare%20Meva/maremeva_v3ghcn.jpg" alt="photo of the restaurant" />
         <h1>Mare Meva</h1>
       </div>
