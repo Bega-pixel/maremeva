dd4e69ab3a4ffb106adc0c51328d2c03be0d0a8d
Bega-pixel <bega2000@protonmail.com>
2022-04-18 12:50:20 +0200

    fix smartphone photo bug

--- maremeva/src/components/cover/Cover.js
+++ maremeva/src/components/cover/Cover.js
@@ -5,7 +5,7 @@ const Cover = () => {
   return (
     
       <div className='cover-container'>
-        <img className='cover-img' src="https://res.cloudinary.com/dazypdtba/image/upload/v1648051351/Mare%20Meva/comida_sqtjni.jpg" alt="photo of the restaurant" />
+        <img className='cover-img' src="https://res.cloudinary.com/dazypdtba/image/upload/v1647432144/Mare%20Meva/maremeva_v3ghcn.jpg" alt="photo of the restaurant" />
         <h1>Mare Meva</h1>
       </div>
     
